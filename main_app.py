77cd08c72cd1a99ae611ea17190bc5fc8433bbe7
Putri Rahmawati <ptrirahma16@gmail.com>
2023-12-25 15:18:24 +0700

    Add data and search button

--- main_app.py
+++ main_app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 
+# Read data
+data = pd.read_csv('data.csv')
+
+# Add title
 st.title('Movies Recommendation System')
+
+# Add search bar for movies
+search = search_term = st.text_input('Enter a name to search:')
