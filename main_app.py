38260226d1a6fa7c7d8930b1045197c930005194
Putri Rahmawati <ptrirahma16@gmail.com>
2023-12-26 13:51:15 +0700

    fix the error

--- main_app.py
+++ main_app.py
@@ -13,10 +13,6 @@ def print_something(index):
     st.write(f"Function called for film with Index: {index}")
 
 
-# Initialize session state
-if 'button_clicked' not in st.session_state:
-    st.session_state.button_clicked = False
-
 # Add title
 st.title('Movies Recommendation System')
 
@@ -46,14 +42,10 @@ if not result.empty:
 
         # Add a button with a unique label
         button_label = f"Find recommendation for {row['title']} (Index: {index})"
-        if st.button(button_label):
+        button_clicked = st.button(button_label, key=index)
+
+        # Check if the button was clicked
+        if button_clicked:
             # Call the function with the film index
             print_something(index)
-            # Set session state to indicate button click
-            st.session_state.button_clicked = True
             # You can add more actions or details for the selected movie using the index
-
-# Check if the button was clicked
-if st.session_state.button_clicked:
-    st.write("Button was clicked!")
-    st.session_state.button_clicked = False  # Reset the session state
