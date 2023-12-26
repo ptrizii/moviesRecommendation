ae8b0844fa3330a33e740c586f3e31cf9a192497
Putri Rahmawati <ptrirahma16@gmail.com>
2023-12-26 12:48:13 +0700

    Add new button

--- main_app.py
+++ main_app.py
@@ -14,21 +14,28 @@ search_term = st.text_input('Enter your favorite film title:')
 
 # Display the result
 # Button for executing the search
+# if st.button('Search'):
+#     # Filter data based on the search term
+#     result = data[data['title_query'].str.contains(search_term, case=False)]
+
+#     # Display search results
+#     st.write('Search Results:')
+#     st.table(result[column_name])
+
+
+# Display the result
+# Button for executing the search
 if st.button('Search'):
     # Filter data based on the search term
-    result = data[data['title_query'].str.contains(search_term, case=False)]
+    result = data[data['Title'].str.contains(search_term, case=False)]
 
-    # Display search results
+# Display search results as buttons
+if result is not None and not result.empty:
     st.write('Search Results:')
-    st.table(result[column_name])
-
-# if not result.empty:
-#     # Display search results as buttons
-#     st.write('Search Results:')
-#     for index, row in result.iterrows():
-#         button_label = row['Title']
-#         if st.button(button_label):
-#             st.write(f"You clicked the button for {button_label}")
-#             # You can add more actions or details for the selected movie
-# else:
-#     st.write('No results found.')
+    for index, row in result.iterrows():
+        button_label = f"{row['Title']} - {row['genres']}"
+        if st.button(button_label):
+            st.write(f"You clicked the button for {row['Title']}")
+            # You can add more actions or details for the selected movie
+else:
+    st.write('No results found.')
