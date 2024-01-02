97919c4365a9b2a97624d6d91aa040c1b50752a7
Putri Rahmawati <ptrirahma16@gmail.com>
2024-01-02 14:05:27 +0700

    try new method

--- main_app.py
+++ main_app.py
@@ -2,15 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from st_files_connection import FilesConnection
+from google.cloud import storage
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("streamlitmovies-bucket/data.csv",
-               input_format="csv", ttl=600)
+# conn = st.connection('gcs', type=FilesConnection)
+# df = conn.read("streamlitmovies-bucket/data.csv",
+#                input_format="csv", ttl=600)
 # Use numpy to load the bytes as an array
 # embd = np.load(BytesIO(npy_bytes))
 data = pd.read_csv('data.csv')
+
+client = storage.Client()
+bucket = client.get_bucket("streamlitmovies-bucket")
+blob = bucket.blob("data.csv")
 
 def main():
     st.title('Movie Recommender System')
