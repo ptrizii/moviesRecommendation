396fdb641aef93959d3e0aeaf35cfcc26674bc91
Putri Rahmawati <ptrirahma16@gmail.com>
2024-01-02 13:43:45 +0700

    add code to retrieve data form gcs

--- main_app.py
+++ main_app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from st_files_connection import FilesConnection
+from google.cloud import storage
+
+# Function to fetch NPY data from Google Cloud Storage
+
+
+def read_npy_data_from_gcs(bucket_name, file_name):
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    data = blob.download_as_bytes()
+    np_array = np.load(io.BytesIO(data))
+    return np_array
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
@@ -23,6 +35,19 @@ def main():
     
     st.write("HI")
 
+    # Streamlit app code
+    st.title("Google Cloud Data in Streamlit")
+
+    # Define Google Cloud Storage details
+    gcs_bucket_name = "your-gcs-bucket-name"
+    gcs_file_name = "complete-embd.npy"
+
+    # Fetch NPY data from Google Cloud Storage
+    npy_data = read_npy_data_from_gcs(gcs_bucket_name, gcs_file_name)
+
+    # Display NPY data in Streamlit
+    st.write("### Data from Google Cloud Storage (NPY)")
+    st.write(npy_data[0])
 
 # Run the Streamlit app
 if __name__ == '__main__':
