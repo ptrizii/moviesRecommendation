de3514a3295aeb756e4de781c3392570ba67f96a
Putri Rahmawati <ptrirahma16@gmail.com>
2024-01-02 12:32:12 +0700

    change code to retrieve data

--- main_app.py
+++ main_app.py
@@ -3,9 +3,14 @@ import pandas as pd
 import numpy as np
 from io import BytesIO
 from st_files_connection import FilesConnection
-
+import os
 from google.cloud import storage
-from io import BytesIO
+import io
+
+# Set the environment variable for Google Cloud credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+
 
 # Set up Google Cloud Storage client
 client = storage.Client()
@@ -14,13 +19,30 @@ client = storage.Client()
 bucket_name = "streamlitmovies-bucket"
 file_path = "streamlitmovies-bucket/complete-embd.npy"
 
-# Retrieve the file from GCS
-bucket = client.get_bucket(bucket_name)
-blob = bucket.blob(file_path)
-npy_bytes = blob.download_as_text()
 
-# Load the .npy file from bytes
-array = np.load(BytesIO(npy_bytes))
+# Function to fetch NPY data from Google Cloud Storage
+
+def read_npy_data_from_gcs(bucket_name, file_name):
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    data = blob.download_as_bytes()
+    np_array = np.load(io.BytesIO(data))
+    return np_array
+
+
+# Streamlit app code
+st.title("Google Cloud Data in Streamlit")
+
+# Define Google Cloud Storage details
+gcs_bucket_name = "streamlitmovies-bucket"
+gcs_file_name = "complete-embd.npy"
+
+# Fetch NPY data from Google Cloud Storage
+npy_data = read_npy_data_from_gcs(gcs_bucket_name, gcs_file_name)
+
+st.write("### Data from Google Cloud Storage (NPY)")
+st.write(npy_data[0])
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
@@ -28,21 +50,21 @@ array = np.load(BytesIO(npy_bytes))
 # npy_bytes = conn.read("streamlitmovies-bucket/complete-embd.npy", ttl=600)
 # Use numpy to load the bytes as an array
 # embd = np.load(BytesIO(npy_bytes))
-data = pd.read_csv('data.csv')
+# data = pd.read_csv('data.csv')
 
-def main():
-    st.title('Movie Recommender System')
+# def main():
+#     st.title('Movie Recommender System')
 
-    movie_list = data['title'].values
+#     movie_list = data['title'].values
 
-    selected_movies = st.selectbox("Type and select your favorite movie", movie_list)
+#     selected_movies = st.selectbox("Type and select your favorite movie", movie_list)
 
-    if st.button("Show Recommendation"):
-        st.write("Here is your recommendation")
+#     if st.button("Show Recommendation"):
+#         st.write("Here is your recommendation")
     
-    st.write("HI")
+#     st.write("HI")
 
 
-# Run the Streamlit app
-if __name__ == '__main__':
-    main()
+# # Run the Streamlit app
+# if __name__ == '__main__':
+#     main()
